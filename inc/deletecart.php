bf77742c1804bcb17d7b4ef976e69e15d73cfd24
aqilanwar <aqilanwarx@gmail.com>
2021-04-17 06:50:01 +0800

    cart

--- inc/deletecart.php
+++ inc/deletecart.php
@@ -3,15 +3,28 @@
     include 'connection.php';
     
     if (isset($_POST['submit'] )!== "") {
+
         $prodid =  $_POST['submit'];
         $userid = $_SESSION['User'];
-        $sql = "DELETE FROM `shopping_cart` WHERE var_product_id = '$prodid' AND username = '$userid' ";
+
+        //Fetch quantity in cart
+        $query = "SELECT quantity FROM shopping_cart WHERE var_product_id = '$prodid' AND username = '$userid'";
+        $result = mysqli_query($sql_connect, $query); 
+        $quantity = mysqli_fetch_assoc($result);
+
+        //Update value after delete from cart
+
+        $update = "UPDATE var_product SET var_product_quan = (var_product_quan + '".$quantity['quantity']."') WHERE var_product_id = '$prodid' " ;
+        $sql2 = mysqli_query($sql_connect, $update);
+
+
+        //Delete from cart
+        $sql = "DELETE FROM shopping_cart WHERE var_product_id = '$prodid' AND username = '$userid' ";
         $delete = mysqli_query($sql_connect, $sql);
 
-        if($sql){
-         header("Location: ../cart.php");
+        header("Location: ../cart.php");
 
-        }
+    
     }
 
     ?>
