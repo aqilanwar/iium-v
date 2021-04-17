72ecf4474c227a183c071fad13a4300a8719e224
aqilanwar <aqilanwarx@gmail.com>
2021-04-18 06:21:05 +0800

    Payment page

--- cart.php
+++ cart.php
@@ -1,10 +1,10 @@
 <?php
-  include 'inc/connection.php';
-  session_start();
-  $user = $_SESSION['User'];
-  if(empty($_SESSION['User'])){
-    header("Location: login.php");
-  }
+include 'inc/connection.php';
+session_start();
+$user = $_SESSION['User'];
+if (empty($_SESSION['User'])) {
+  header("Location: login.php");
+}
 ?>
 
 
@@ -21,7 +21,7 @@
   <meta content="" name="keywords">
 
   <!-- Favicons -->
-  <link href="https://fonts.googleapis.com/icon?family=Material+Icons"rel="stylesheet">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
   <link href="assets/img/ipm-logo-01.png" rel="icon">
   <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -46,17 +46,17 @@
   <!-- ======= Header ======= -->
   <?php include 'inc/header.php' ?>
   <!-- ======= Hero Section ======= -->
-    <div class="container" style="margin-top:150px; min-height:90vh;">
-      <h1>Shopping Cart</h1>
-        <div class="container">
+  <div class="container" style="margin-top:150px; min-height:90vh;">
+    <h1>Shopping Cart</h1>
+    <div class="container">
 
-            <tbody>
-            <?php 
-                $query = "SELECT * FROM shopping_cart WHERE username = '$user' ";
-                $result = mysqli_query($sql_connect, $query); 
-                $total = 0 ;
-                if(mysqli_num_rows($result)== 0){
-                  echo '
+      <tbody>
+        <?php
+        $query = "SELECT * FROM shopping_cart WHERE username = '$user' ";
+        $result = mysqli_query($sql_connect, $query);
+        $total = 0;
+        if (mysqli_num_rows($result) == 0) {
+          echo '
                           <div class="card-body cart">
                             <div class="col-sm-12 empty-cart-cls text-center"> <img src="assets/img/empty.png" width="100" height="100" class="img-fluid mb-4 mr-3">
                               <h3><strong>Your Cart is Empty :(</strong></h3>
@@ -64,9 +64,9 @@
                               <a href="product.php" class="btn btn-primary cart-btn-transform m-3" data-abc="true">Product </a>
                             </div>
                            </div>
-                  ' ;
-                }else{
-                  echo '
+                  ';
+        } else {
+          echo '
                   <table class="table">
                   <thead class="thead-light">
                       <tr>
@@ -80,83 +80,82 @@
                       </tr>
                   </thead>
                   ';
-                }
+        }
 
-                while ($cart = mysqli_fetch_assoc($result)) {
+        while ($cart = mysqli_fetch_assoc($result)) {
 
-                    $query2 = "SELECT * FROM var_product WHERE var_product_id = '".$cart['var_product_id']."' " ;
-                    $result2 = mysqli_query($sql_connect,$query2);
-                    $cart2 = mysqli_fetch_assoc($result2);
+          $query2 = "SELECT * FROM var_product WHERE var_product_id = '" . $cart['var_product_id'] . "' ";
+          $result2 = mysqli_query($sql_connect, $query2);
+          $cart2 = mysqli_fetch_assoc($result2);
 
-                    $query3 = "SELECT * FROM pic_product WHERE product_id = '".$cart['product_id']."' ";
-                    $result3 = mysqli_query($sql_connect,$query3);
-                    $cart3 = mysqli_fetch_assoc($result3);
+          $query3 = "SELECT * FROM pic_product WHERE product_id = '" . $cart['product_id'] . "' ";
+          $result3 = mysqli_query($sql_connect, $query3);
+          $cart3 = mysqli_fetch_assoc($result3);
 
-                    $query4 = "SELECT * FROM product WHERE product_id = '".$cart['product_id']."' ";
-                    $result4 = mysqli_query($sql_connect,$query4);
-                    $cart4 = mysqli_fetch_assoc($result4);
-            ?>
-                <tr>
-                    <th scope="row" style="text-align:center; vertical-align:middle;">
-                    <form action="inc/deletecart.php" method="POST">
-                      <button type="submit" name="submit" id="submit"  value ="<?php echo $cart['var_product_id']; ?>" class="btn btn-danger">
-                          <span class="material-icons align-middle">
-                              highlight_off
-                          </span>
-                      </button>
-                    </form>
-                    </th>
-                    <td width=200px; style="text-align:center;">
-                        <img src="images/<?php echo $cart3['pic_name'] ?>" height=100px; alt="">
-                    </td>
-                    <td style="vertical-align:middle;">
-                        <p><a href="view.php?view_prod=<?php echo $cart['product_id'] ?>"><?php echo $cart4['product_title'] ?></a></p>
-                        <p><?php echo 'Variation : ' ,  $cart2['var_product_title']; ?></p>
-                    </td>
-                    <td style="vertical-align:middle;">
-                        <?php echo 'RM ', $cart2['var_product_price']; ?>
-                    </td>
-                    <td style="vertical-align:middle;">
-                        <?php echo $cart['quantity']; ?>
-                    </td>
-                    <td style="vertical-align:middle;">
-                        
-                        <?php $subtotal = $cart2['var_product_price'] * $cart['quantity'] ; 
-                            $subtotal = number_format($subtotal,2) ;
-                            echo 'RM ' , $subtotal;
-                        ?>
-                    </td>
-                    <td style="vertical-align:middle;">
-                        <a href=""><?php echo $cart4['user_id'] ?> </a>
-                    </td>
-                </tr> 
+          $query4 = "SELECT * FROM product WHERE product_id = '" . $cart['product_id'] . "' ";
+          $result4 = mysqli_query($sql_connect, $query4);
+          $cart4 = mysqli_fetch_assoc($result4);
+        ?>
+          <tr>
+            <th scope="row" style="text-align:center; vertical-align:middle;">
+              <form action="inc/deletecart.php" method="POST">
+                <button type="submit" name="submit" id="submit" value="<?php echo $cart['var_product_id']; ?>" class="btn btn-danger">
+                  <span class="material-icons align-middle">
+                    highlight_off
+                  </span>
+                </button>
+              </form>
+            </th>
+            <td width=200px; style="text-align:center;">
+              <img src="images/<?php echo $cart3['pic_name'] ?>" height=100px; alt="">
+            </td>
+            <td style="vertical-align:middle;">
+              <p><a href="view.php?view_prod=<?php echo $cart['product_id'] ?>"><?php echo $cart4['product_title'] ?></a></p>
+              <p><?php echo 'Variation : ',  $cart2['var_product_title']; ?></p>
+            </td>
+            <td style="vertical-align:middle;">
+              <?php echo 'RM ', $cart2['var_product_price']; ?>
+            </td>
+            <td style="vertical-align:middle;">
+              <?php echo $cart['quantity']; ?>
+            </td>
+            <td style="vertical-align:middle;">
 
-                <?php
-                    $total = $total + $subtotal ;
-                    $total = number_format($total,2);
+              <?php $subtotal = $cart2['var_product_price'] * $cart['quantity'];
+              $displaysubtotal = number_format($subtotal, 2);
+              echo 'RM ', $displaysubtotal;
+              ?>
+            </td>
+            <td style="vertical-align:middle;">
+              <a href=""><?php echo $cart4['user_id'] ?> </a>
+            </td>
+          </tr>
 
-                  }
-                ?>
-            </tbody>
-            </table>
-            <?php 
-               if(mysqli_num_rows($result) > 0){
-            ?>
-            <div class="container" style="margin-bottom:200px;">
-              <div class="card" style="width: 30rem; float:right;">
-                <div class="card-body" style="flex-direction:column">
-                  <h5 class="card-title">Grand Total : <?php echo'RM ' , $total ?></h5>
-                  <hr>
-                  <a href="payment/payment.php" class="btn btn-primary">Checkout</a>
-                </div>
-              </div>
+        <?php
+          $total = $total + $subtotal;
+          $total = number_format($total, 2);
+        }
+        ?>
+      </tbody>
+      </table>
+      <?php
+      if (mysqli_num_rows($result) > 0) {
+      ?>
+        <div class="container" style="margin-bottom:200px;">
+          <div class="card" style="width: 30rem; float:right;">
+            <div class="card-body" style="flex-direction:column">
+              <h5 class="card-title">Grand Total : <?php echo 'RM ', $total ?></h5>
+              <hr>
+              <a href="payment/payment.php" class="btn btn-primary">Checkout</a>
             </div>
-            <?php } ?>
+          </div>
         </div>
+      <?php } ?>
     </div>
+  </div>
 
 
-    
+
   <!-- ======= Footer ======= -->
   <footer id="footer" class="footer">
     <div class="container">
--- payment/payment.php
+++ payment/payment.php
@@ -1,141 +1,148 @@
 <?php
-  include '../inc/connection.php';
-  session_start();
-  $user = $_SESSION['User'];
-  if(empty($_SESSION['User'])){
-    header("Location: login.php");
-  }
+include '../inc/connection.php';
+session_start();
+$user = $_SESSION['User'];
+if (empty($_SESSION['User'])) {
+  header("Location: login.php");
+}
 ?>
 
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Podcast4u : : Payment</title>
-    <link rel="icon" href="images/logo.png" type="image/gif" />
-    <link rel="stylesheet" href="style.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Podcast4u : : Payment</title>
+  <link rel="icon" href="images/logo.png" type="image/gif" />
+  <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-    <nav class="navbar navbar-dark bg-dark" style="background-color: #000;">
-        <a class="navbar-brand" href="../index.php">
-          <img src="images/logo.png" style=" filter: invert(100%);-webkit-filter: invert(100%); margin-left: 40px;" width=150px; alt="">
-        </a>
-      </nav>
-    <div class='container'>
-        <div class='window'>
-          <div class='order-info'>
-            <div class='order-info-content'>
-              <h1 style="text-align: center;">Order Summary</h1>
-                      <hr>
-                      <div class="cartNav">
+  <nav class="navbar navbar-dark bg-dark" style="background-color: #000;">
+    <a class="navbar-brand" href="../index.php">
+      <img src="images/logo.png" style=" filter: invert(100%);-webkit-filter: invert(100%); margin-left: 40px;" width=150px; alt="">
+    </a>
+  </nav>
+  <div class='container'>
+    <div class='window'>
+      <div class='order-info'>
+        <div class='order-info-content'>
+          <h1 style="text-align: center;">Order Summary</h1>
+          <hr>
+          <div class="cartNav">
 
-                      <?php 
-                        $query = "SELECT * FROM shopping_cart WHERE username = '$user' ";
-                        $result = mysqli_query($sql_connect, $query); 
-                        $total = 0 ;
+            <?php
+            $query = "SELECT * FROM shopping_cart WHERE username = '$user' ";
+            $result = mysqli_query($sql_connect, $query);
+            $total = 0;
 
-                        while ($cart = mysqli_fetch_assoc($result)) {
+            while ($cart = mysqli_fetch_assoc($result)) {
 
-                          $query2 = "SELECT * FROM var_product WHERE var_product_id = '".$cart['var_product_id']."' " ;
-                          $result2 = mysqli_query($sql_connect,$query2);
-                          $cart2 = mysqli_fetch_assoc($result2);
-      
-                          $query3 = "SELECT * FROM pic_product WHERE product_id = '".$cart['product_id']."' ";
-                          $result3 = mysqli_query($sql_connect,$query3);
-                          $cart3 = mysqli_fetch_assoc($result3);
-      
-                          $query4 = "SELECT * FROM product WHERE product_id = '".$cart['product_id']."' ";
-                          $result4 = mysqli_query($sql_connect,$query4);
-                          $cart4 = mysqli_fetch_assoc($result4);
-                      ?>                      
-                        <table class='order-table'>
-                          <tbody>
-                            <tr>
-                              <td><img src='../images/<?php echo $cart3['pic_name'] ?>' class='full-width'></img>
-                              </td>
-                              <td>
-                                <br><span class='thin'><?php echo $cart4['product_title'] ?></span>
-                                <br><small><?php echo 'Variation : ' ,  $cart2['var_product_title']; ?><br> <span class='thin small'><?php echo 'RM ', $cart2['var_product_price']; ?></small><br><span class='thin small'><?php echo 'Quantity : ' , $cart['quantity']; ?><br></span>
-                              </td>
-                            </tr>
-                            <tr>
-                              <td>
-                                <div class='price'>                       
-                                   <?php $subtotal = $cart2['var_product_price'] * $cart['quantity'] ; 
-                                      $subtotal = number_format($subtotal,2) ;
-                                      echo 'RM ' , $subtotal;
-                                  ?>
-                                </div>
-                              </td>
+              $query2 = "SELECT * FROM var_product WHERE var_product_id = '" . $cart['var_product_id'] . "' ";
+              $result2 = mysqli_query($sql_connect, $query2);
+              $cart2 = mysqli_fetch_assoc($result2);
 
-                            </tr>
-                          </tbody>
-                        </table>
+              $query3 = "SELECT * FROM pic_product WHERE product_id = '" . $cart['product_id'] . "' ";
+              $result3 = mysqli_query($sql_connect, $query3);
+              $cart3 = mysqli_fetch_assoc($result3);
 
-                        <hr>  
-                      <?php 
-                        $total = $total + $subtotal ;
-                        $total = number_format($total,2);
-                      
-                      }
-                       ?>
-                      <table class='order-table' style="margin-bottom:20px; text-align:center;">
-                        <tbody>
-                          <tr>
-                            <td>
-                              </td>
-                              <td>
-                                <h3>TOTAL : <?php echo'RM ' , $total ?></h3>
-                              </td>
-                            </tr>
-                          </tbody>
-                        </table>
-                          <hr>
-                      </div>   
-            </div>
-      </div>
-
-              <div class='credit-info'>
-                <div class='credit-info-content'>
-                  <table class='half-input-table'>
-                    <tr><td>Please select your card: </td><td><div class='dropdown' id='card-dropdown'><div class='dropdown-btn' id='current-card'>Visa</div>
-                      <div class='dropdown-select'>
-                      <ul>
-                        <li>Master Card</li>
-                        <li>American Express</li>
-                        </ul></div>
+              $query4 = "SELECT * FROM product WHERE product_id = '" . $cart['product_id'] . "' ";
+              $result4 = mysqli_query($sql_connect, $query4);
+              $cart4 = mysqli_fetch_assoc($result4);
+            ?>
+              <table class='order-table'>
+                <tbody>
+                  <tr>
+                    <td><img src='../images/<?php echo $cart3['pic_name'] ?>' class='full-width'></img>
+                    </td>
+                    <td>
+                      <br><span class='thin'><?php echo $cart4['product_title'] ?></span>
+                      <br><small><?php echo 'Variation : ',  $cart2['var_product_title']; ?><br> <span class='thin small'><?php echo 'RM ', $cart2['var_product_price']; ?></small><br><span class='thin small'><?php echo 'Quantity : ', $cart['quantity']; ?><br></span>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <div class='price'>
+                        <?php $subtotal = $cart2['var_product_price'] * $cart['quantity'];
+                        $displaysubtotal = number_format($subtotal, 2);
+                        echo 'RM ', $displaysubtotal;
+                        ?>
                       </div>
-                     </td></tr>
-                  </table>
-                  <img src='https://dl.dropboxusercontent.com/s/ubamyu6mzov5c80/visa_logo%20%281%29.png' height='80' class='credit-card-image' id='credit-card-image'></img>
-                  Email Adress
-                  <form action="pay.php">
-                    <input type="email" class='input-field' required></input>
-                    Card Number
-                    <input type="text" maxlength="16" class='input-field'></input>
-                    Card Holder Name
-                    <input type="text" class='input-field'></input>
-                  
-                  <table class='half-input-table'>
-                    <tr>
-                      <td> Expires
-                        <input type="text" class='input-field'></input>
-                      </td>
-                      <td>CVC
-                        <input class='input-field'></input>
-                      </td>
-                    </tr>
-                  </table>
-                  <button class='pay-btn'>Checkout</button>
-                  </form>
+                    </td>
 
-                </div>
-      
-              </div>
-            </div>
+                  </tr>
+                </tbody>
+              </table>
+
+              <hr>
+            <?php
+              $total = $total + $subtotal;
+              $total = number_format($total, 2);
+            }
+            ?>
+            <table class='order-table' style="margin-bottom:20px; text-align:center;">
+              <tbody>
+                <tr>
+                  <td>
+                  </td>
+                  <td>
+                    <h3>TOTAL : <?php echo 'RM ', $total ?></h3>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+            <hr>
+          </div>
+        </div>
       </div>
+
+      <div class='credit-info'>
+        <div class='credit-info-content'>
+          <table class='half-input-table'>
+            <tr>
+              <td>Please select your card: </td>
+              <td>
+                <div class='dropdown' id='card-dropdown'>
+                  <div class='dropdown-btn' id='current-card'>Visa</div>
+                  <div class='dropdown-select'>
+                    <ul>
+                      <li>Master Card</li>
+                      <li>American Express</li>
+                    </ul>
+                  </div>
+                </div>
+              </td>
+            </tr>
+          </table>
+          <img src='https://dl.dropboxusercontent.com/s/ubamyu6mzov5c80/visa_logo%20%281%29.png' height='80' class='credit-card-image' id='credit-card-image'></img>
+          Email Adress
+          <form action="pay.php">
+            <input type="email" class='input-field' required></input>
+            Card Number
+            <input type="text" maxlength="16" class='input-field'></input>
+            Card Holder Name
+            <input type="text" class='input-field'></input>
+
+            <table class='half-input-table'>
+              <tr>
+                <td> Expires
+                  <input type="text" class='input-field'></input>
+                </td>
+                <td>CVC
+                  <input class='input-field'></input>
+                </td>
+              </tr>
+            </table>
+            <button class='pay-btn'>Checkout</button>
+          </form>
+
+        </div>
+
+      </div>
+    </div>
+  </div>
 </body>
 <script src="payment.js"> </script>
 
